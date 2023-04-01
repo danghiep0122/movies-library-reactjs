1c81bd975b57f3ec61d4cf5340d3e2f65e8447cd
danghiep <danghiep0122@gmail.com>
2023-04-01 17:11:08 +0700

    done homepage

--- src/pages/homePage/index.jsx
+++ src/pages/homePage/index.jsx
@@ -1,22 +1,37 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 import './styles.scss';
 import SearchForm from '../../components/search';
 import TrendingCredits from '../../components/topTrending';
 import TrailerCredits from '../../components/trailerCredits';
-import TrailerModal from '../../components/trailerModal';
-import { useState } from 'react';
+import Image from '../../components/image';
 
 export default function HomePage() {
-  const [toggle, setToggle] = useState(false);
+  const [bannerUrl, setBannerUrl] = useState('');
+  const apiKey = process.env.REACT_APP_API_KEY;
+  const url = `https://api.themoviedb.org/3/trending/all/day?api_key=${apiKey}`;
+  const getBannerImg = async (random) => {
+    await axios
+      .get(url)
+      .then((res) => setBannerUrl(res.data.results[random].backdrop_path))
+      .catch((error) => console.error(error));
+  };
+
+  useEffect(() => {
+    const random = Math.floor(Math.random(0, 19) * 10);
+    getBannerImg(random);
+  }, []);
   return (
     <main className="homepage-main">
       <div className="homepage-body">
         <section className="search-all-form">
           <div className="background-image-wrapper">
-            <img src="https://images.hdqwalls.com/wallpapers/rog-logo-red-4k-ah.jpg" alt="demo" />
+            <Image srcfull={bannerUrl} alt={bannerUrl} />
           </div>
           <div>
             <div className="homepage-greeting">
-              <h2 onClick={() => setToggle(true)}>Welcome.</h2>
+              <h2>Welcome.</h2>
               <h3>Millions of movies, TV shows and people to discover. Explore now.</h3>
             </div>
             <SearchForm />
@@ -28,11 +43,6 @@ export default function HomePage() {
         <section className="homepage-trailer-section">
           <TrailerCredits />
         </section>
-        {toggle && (
-          <section>
-            <TrailerModal setToggle={setToggle} />
-          </section>
-        )}
       </div>
     </main>
   );
