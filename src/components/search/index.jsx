744035e1b3a255e30e933a50eec5c7415a1128fb
danghiep <danghiep0122@gmail.com>
2023-04-05 13:51:41 +0700

    done interface

--- src/components/search/index.jsx
+++ src/components/search/index.jsx
@@ -8,7 +8,8 @@ import {
   MaleIcon,
   MovieIcon,
   TVShowIcon,
-  UserIcon
+  UserIcon,
+  SearchFailedIcon
 } from '../../assets/img/icon/allIcon';
 import useDebounce from '../../hooks/useDebounce';
 import './styles.scss';
@@ -80,7 +81,10 @@ const SearchForm = () => {
       <section className="search-component">
         <div
           className={
-            allMovie.length === 0 && allTvShow.length === 0 && allPerson.length === 0
+            allMovie.length === 0 &&
+            allTvShow.length === 0 &&
+            allPerson.length === 0 &&
+            input === ''
               ? 'disappear'
               : 'modal'
           }
@@ -96,91 +100,98 @@ const SearchForm = () => {
           />
           <div className="right-component">
             <div onClick={handleOffModal} className="delete-icon">
-              <p>Close</p>
+              <p>Clear all</p>
               <DeleteBtn height="1.6rem" width="1.6rem" fill="var(--text-dark-color)" />
             </div>
           </div>
         </form>
       </section>
-      <section className="people-result">
-        <div
-          className={
-            allMovie.length === 0 && allTvShow.length === 0 && allPerson.length === 0
-              ? 'disappear'
-              : 'result-section'
-          }
-        >
-          <div className="result-list">
-            <div className="media-type-title-and-logo">
-              <MovieIcon width="2rem" height="2rem" fill="var(--red-color)" />
-              <h1 style={{ color: 'var(--red-color)' }}>Movies</h1>
-            </div>
-            {allMovie.length === 0 ? (
-              <div>No result</div>
-            ) : (
-              allMovie.map(({ id, title, release_date, original_language }) => (
-                <Link to={`./movie/${id}`} key={id}>
-                  <div className="result-item">
-                    <h3>{title}</h3>
-                    <h3 style={{ textTransform: 'uppercase' }}>{`(${original_language})`}</h3>
-                    <h4>{release_date}</h4>
-                  </div>
-                </Link>
-              ))
-            )}
+      <section
+        className={
+          allMovie.length === 0 && allTvShow.length === 0 && allPerson.length === 0 && input === ''
+            ? 'disappear'
+            : 'result-section'
+        }
+      >
+        <div className="result-list">
+          <div className="media-type-title-and-logo">
+            <MovieIcon width="2rem" height="2rem" fill="var(--red-color)" />
+            <h1 style={{ color: 'var(--red-color)' }}>Movies</h1>
           </div>
-          <div className="result-list">
-            <div className="media-type-title-and-logo">
-              <TVShowIcon width="2rem" height="2rem" fill="var(--blue-color)" />
-              <h1 style={{ color: 'var(--blue-color)' }}>TV Show</h1>
+          {allMovie.length === 0 ? (
+            <div className="no-result-alert">
+              <SearchFailedIcon width="2rem" height="2rem" />
+              No result
             </div>
-            {allTvShow.length === 0 ? (
-              <div>No result</div>
-            ) : (
-              allTvShow.map(({ id, name, first_air_date, original_language }) => (
-                <Link to={`./tv/${id}`} key={id}>
-                  <div className="result-item">
-                    <h3>{name}</h3>
-                    <h3 style={{ textTransform: 'uppercase' }}>{`(${original_language})`}</h3>
-                    <h4>{first_air_date}</h4>
-                  </div>
-                </Link>
-              ))
-            )}
+          ) : (
+            allMovie.map(({ id, title, release_date, original_language }) => (
+              <Link to={`./movie/${id}`} key={id}>
+                <div className="result-item">
+                  <h3>{title}</h3>
+                  <h3 style={{ textTransform: 'uppercase' }}>{`(${original_language})`}</h3>
+                  <h4>{release_date}</h4>
+                </div>
+              </Link>
+            ))
+          )}
+        </div>
+        <div className="result-list">
+          <div className="media-type-title-and-logo">
+            <TVShowIcon width="2rem" height="2rem" fill="var(--blue-color)" />
+            <h1 style={{ color: 'var(--blue-color)' }}>TV Show</h1>
           </div>
+          {allTvShow.length === 0 ? (
+            <div className="no-result-alert">
+              <SearchFailedIcon width="2rem" height="2rem" />
+              No result
+            </div>
+          ) : (
+            allTvShow.map(({ id, name, first_air_date, original_language }) => (
+              <Link to={`./tv/${id}`} key={id}>
+                <div className="result-item">
+                  <h3>{name}</h3>
+                  <h3 style={{ textTransform: 'uppercase' }}>{`(${original_language})`}</h3>
+                  <h4>{first_air_date}</h4>
+                </div>
+              </Link>
+            ))
+          )}
+        </div>
 
-          <div className="result-list">
-            <div className="media-type-title-and-logo">
-              <UserIcon width="2rem" height="2rem" fill="var(--primary-color)" />
-              <h1 style={{ color: 'var(--primary-color)' }}>People</h1>
-            </div>
-            {allPerson.length === 0 ? (
-              <div>No result</div>
-            ) : (
-              allPerson.map(({ id, name, profile_path, gender, known_for_department }) => (
-                <Link to={`./person/${id}`} key={id}>
-                  <div className="result-item">
-                    <div className="people-avatar">
-                      {profile_path ? (
-                        <Image src={profile_path} alt={name} />
-                      ) : (
-                        <QuestionMarkIcon height="40" width="40" fill="#333" />
-                      )}
-                    </div>
-                    <h3>{name}</h3>
-                    <div className="people-gender">
-                      {gender === 2 ? (
-                        <MaleIcon width="1rem" height="1rem" fill="blue" />
-                      ) : (
-                        <FemaleIcon width="1rem" height="1rem" fill="#ff748c" />
-                      )}
-                    </div>
-                    <p>{known_for_department}</p>
-                  </div>
-                </Link>
-              ))
-            )}
+        <div className="result-list">
+          <div className="media-type-title-and-logo">
+            <UserIcon width="2rem" height="2rem" fill="var(--primary-color)" />
+            <h1 style={{ color: 'var(--primary-color)' }}>People</h1>
           </div>
+          {allPerson.length === 0 ? (
+            <div className="no-result-alert">
+              <SearchFailedIcon width="2rem" height="2rem" />
+              No result
+            </div>
+          ) : (
+            allPerson.map(({ id, name, profile_path, gender, known_for_department }) => (
+              <Link to={`./person/${id}`} key={id}>
+                <div className="result-item">
+                  <div className="people-avatar">
+                    {profile_path ? (
+                      <Image src={profile_path} alt={name} />
+                    ) : (
+                      <QuestionMarkIcon height="40" width="40" fill="#333" />
+                    )}
+                  </div>
+                  <h3>{name}</h3>
+                  <div className="people-gender">
+                    {gender === 2 ? (
+                      <MaleIcon width="1rem" height="1rem" fill="blue" />
+                    ) : (
+                      <FemaleIcon width="1rem" height="1rem" fill="#ff748c" />
+                    )}
+                  </div>
+                  <p>{known_for_department}</p>
+                </div>
+              </Link>
+            ))
+          )}
         </div>
       </section>
     </main>
